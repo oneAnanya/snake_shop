10e82cd450302d0bb9499120acfc9f54ad1f752b
OneAnanya <ananyamohapatra61@gmail.com>
2025-06-19 22:27:22 +0530

    underprocess

---
+++ SnakeShop/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SnakeShop project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SnakeShop.settings')
+
+application = get_asgi_application()
